bf2c3999606b8225b11168128d8bf9c4b30f6d04
wanatotry <81295080+wanatotry@users.noreply.github.com>
2024-01-21 12:31:15 +0200

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,9 +1,3 @@
 import {Inter, Lusitana } from 'next/font/google';
 
 export const inter = Inter({subsets: ['latin']});
-
-export const lustiana = Lusitana({
-    weight: ['400', '700'],
-    subsets: ['latin'],
-    display: 'swap',
-})
