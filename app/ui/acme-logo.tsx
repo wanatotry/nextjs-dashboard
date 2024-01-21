0d16330e00acbebec695438086c7513e59bfc871
wanatotry <81295080+wanatotry@users.noreply.github.com>
2024-01-21 12:39:10 +0200

    Update acme-logo.tsx

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,10 +1,9 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lustiana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lustiana.className} flex flex-row items-center leading-none text-white`}
+      className={`flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
